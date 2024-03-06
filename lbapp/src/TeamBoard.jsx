5a259db10db706de2fa327f2fc4ac8b3a82570d3
shaik11r <nadeencool11@gmail.com>
2024-03-06 23:51:46 +0530

    done teamBoard

---
+++ lbapp/src/TeamBoard.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { easeIn, motion } from "framer-motion";
+const TeamBoard = () => {
+  const [teamA, setTeamA] = useState(parseInt(localStorage.getItem("teamA")) || 0);
+  const [teamB, setTeamB] = useState(parseInt(localStorage.getItem("teamB")) || 0);
+
+  const handleClickA = () => {
+    const newTeamAScore = teamA + 1;
+    setTeamA(newTeamAScore);
+    localStorage.setItem("teamA", newTeamAScore);
+    console.log(teamA);
+  };
+  const handleClickB = () => {
+    const newTeamBScore = teamB + 1;
+    setTeamB(newTeamBScore);
+    localStorage.setItem("teamB", newTeamBScore);
+    console.log(teamB);
+  };
+  const handleCleanup = () => {
+    setTeamA(0);
+    setTeamB(0);
+    localStorage.setItem("teamA", 0);
+    localStorage.setItem("teamB", 0);
+  };
+  return (
+    <>
+      <div className=" m-11 font-mono text-center">
+        <h1 className="text-2xl p-3 mb-3">Team ScoreBoard💯</h1>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          transition={{ ease: "easeOut", duration: 0.2 }}
+          className="my-10 rounded border border-red-500 p-2 text-xl"
+          onClick={handleCleanup}>
+          CleanStats🧹
+        </motion.button>
+      </div>
+      <div className="grid place-items-center grid-cols-2 font-mono ">
+        <div className="m-3">
+          <h1 className="text-xl mb-3 text-red-600">Team 1</h1>
+          <p>
+            You belong to team 1 if your name starts with:
+            <br />
+            <p className="text-xl">A, C, E, G, I, K, M, O, Q, S, U, W, Y</p>
+          </p>
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ ease: "easeOut", duration: 0.2 }}
+            className="my-5 border border-purple-600 p-3 rounded text-xl"
+            onClick={handleClickA}>
+            {`Score-${teamA} 🛺`}
+          </motion.button>
+        </div>
+        <div className="m-3">
+          <h1 className="text-xl mb-3 text-blue-600">Team 2</h1>
+          <p>
+            You belong to team 2 if your name starts with: <br />
+            <p className="text-xl"> B, D, F, H, J, L, N, P, R, T, V, X, Z</p>
+          </p>
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ ease: "easeOut", duration: 0.2 }}
+            className="my-5 border border-green-600 p-3 rounded text-xl"
+            onClick={handleClickB}>
+            {`Score-${teamB} 🚗`}
+          </motion.button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TeamBoard;
